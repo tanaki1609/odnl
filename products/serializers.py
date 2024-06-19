095f01176f4fe4a1f4b8619e09b1ae1cdaebc8fe
tanaki <tanaki.93@gmail.com>
2024-06-19 06:35:56 +0600

    lesson 4

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Category, Tag
+from rest_framework.exceptions import ValidationError
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -27,3 +28,33 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_tag_names(self, product):
         return [tag.name for tag in product.tags.all()]
+
+
+class ProductValidateSerializer(serializers.Serializer):
+    title = serializers.CharField(min_length=5, max_length=100)
+    price = serializers.FloatField(min_value=1, max_value=1000000)
+    description = serializers.CharField(required=False)
+    is_active = serializers.BooleanField()
+    category_id = serializers.IntegerField(min_value=1)
+    tags = serializers.ListField(child=serializers.IntegerField(min_value=1))
+
+    # def validate(self, attrs):
+    #     category_id = attrs['category_id']
+    #     try:
+    #         Category.objects.get(id=category_id)
+    #     except Category.DoesNotExist:
+    #         raise ValidationError('Category not found')
+    #     return attrs
+
+    def validate_category_id(self, category_id):
+        try:
+            Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            raise ValidationError('Category not found')
+        return category_id
+
+    def validate_tags(self, tags):  # [1,2,100]
+        tags_from_db = Tag.objects.filter(id__in=tags)  # [1,2]
+        if len(tags_from_db) != len(tags):
+            raise ValidationError('Tag not found')
+        return tags
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductValidateSerializer
 from .models import Product
 
 
@@ -17,13 +17,19 @@ def product_list_create_api_view(request):
         # step 3. return list of dictionary
         return Response(data=list_)
     elif request.method == 'POST':
-        # step 1: Receive data from request body
-        title = request.data.get('title')
-        price = request.data.get('price')
-        description = request.data.get('description')
-        is_active = request.data.get('is_active')
-        category_id = request.data.get('category_id')
-        tags = request.data.get('tags')
+        # step 0: Validate data
+        serializer = ProductValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data={'errors': serializer.errors})
+
+        # step 1: Receive data from Serializer Validated data
+        title = serializer.validated_data.get('title')  # None
+        price = serializer.validated_data.get('price')  # None
+        description = serializer.validated_data.get('description')
+        is_active = serializer.validated_data.get('is_active')
+        category_id = serializer.validated_data.get('category_id')
+        tags = serializer.validated_data.get('tags')
 
         # step 2: Create product by this data
         product = Product.objects.create(
@@ -51,12 +57,15 @@ def product_detail_api_view(request, id):
         data = ProductSerializer(product).data
         return Response(data=data)
     elif request.method == 'PUT':
-        product.title = request.data.get('title')
-        product.description = request.data.get('description')
-        product.is_active = request.data.get('is_active')
-        product.price = request.data.get('price')
-        product.category_id = request.data.get('category_id')
-        product.tags.set(request.data.get('tags'))
+        serializer = ProductValidateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        product.title = serializer.validated_data.get('title')
+        product.description = serializer.validated_data.get('description')
+        product.is_active = serializer.validated_data.get('is_active')
+        product.price = serializer.validated_data.get('price')
+        product.category_id = serializer.validated_data.get('category_id')
+        product.tags.set(serializer.validated_data.get('tags'))
         product.save()
         return Response(status=status.HTTP_201_CREATED)
     elif request.method == 'DELETE':
